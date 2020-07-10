eb00566524a50eabcd6349cacb12a89eeba3bf00
fthomp8044 <fthomp2430@gmail.com>
2020-07-10 18:32:54 -0400

    boiler plate

---
+++ src/components/User.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="component">
+        <h1>The User Component</h1>
+        <p>I'm an awesome User!</p>
+        <button v-on:click="changeName">Change my name</button>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <app-user-detail :myName="name" @nameWasReset="name = $event" :resentFn="resetName" :userAge="age"></app-user-detail>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+              <!-- pass userAge as a prop  -->
+                <app-user-edit :userAge="age" @ageWasEdited="age = $event"></app-user-edit>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import UserDetail from './UserDetail.vue';
+    import UserEdit from './UserEdit.vue';
+
+    export default {
+
+        data : function() {
+          return {
+            name: 'Max',
+            age: 26
+          };
+        },
+        methods: {
+          changeName() {
+            this.name = "Ana";
+          }
+        },
+        components: {
+            appUserDetail: UserDetail,
+            appUserEdit: UserEdit
+        }
+    }
+</script>
+
+<style scoped>
+    div {
+        background-color: lightblue;
+    }
+</style>
